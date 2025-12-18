467a2095b9b76f52d1e6f0365287fc75e54308f7
hammadreal02 <muhammadhammadhammad588@gmail.com>
2025-12-18 05:00:17 +0500

    Fix: Corrected baseUrl in docusaurus.config.ts for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://hammadreal02.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-and-robotic/',
+  baseUrl: '/Physical-AI-and-Robotics/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
